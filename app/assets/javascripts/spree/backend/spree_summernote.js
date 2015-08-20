efd8173486db28d782119e834cc09d4775567a22
Dom Stockdale <doms.stockdale@gmail.com>
2015-08-20 08:46:47 +0100

    Includes video stuff

--- app/assets/javascripts/spree/backend/spree_summernote.js
+++ app/assets/javascripts/spree/backend/spree_summernote.js
@@ -1,3 +1,4 @@
 //= require spree_summernote/summernote
+//= require spree_summernote/summernote-ext-video
 //= require spree_summernote/summernote_locales
 //= require spree_summernote/summernote_settings
